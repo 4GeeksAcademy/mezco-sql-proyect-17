81355f4acf994617b1da6013f996f06d229351e7
Beatriz Solana <solana93@gmail.com>
2025-05-19 09:02:00 +0000

    fin

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -61,13 +61,33 @@ LIMIT 10;
 
 
 
--- MISSION 6 We want to identify the most active observers. Who are the people with the most observation records?
+-- MISSION 6 We want to identify the most active observers. Who are the people with the most observation records?;
+
+SELECT o.observer, 
+COUNT(*) AS total_records
+FROM observations o
+GROUP BY o.observer
+ORDER BY total_records DESC
+LIMIT 10;
 
 
+-- MISSION 7 What species have never been observed? Check if there are species in the table species that do not appear in observations;
 
--- MISSION 7
--- Your query here:
+SELECT 
+  s.common_name, 
+  s.scientific_name
+FROM species s
+LEFT JOIN observations o ON s.id = o.species_id
+WHERE o.id IS NULL;
+
+-- MISSION 8 On which dates were the most distinct species observed? This information is useful to explore peak biodiversity on specific days;
+
+SELECT 
+  observation_date,
+  COUNT(DISTINCT species_id) AS distinct_species_count
+FROM observations
+GROUP BY observation_date
+ORDER BY distinct_species_count DESC
+LIMIT 10;
 
 
--- MISSION 8
--- Your query here:
